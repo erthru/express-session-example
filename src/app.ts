282728e98238af3498284f905e4e284b7c2640de
Suprianto Djamalu <erthru@Supriantos-MacBook-Pro.local>
2021-02-24 23:17:10 +0800

    mongodb store added as session store

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,24 @@ import cors from "cors";
 import routes from "./routes";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import MongoDBStore from "connect-mongodb-session";
 
 const app = express();
+const PORT = 3003;
+const DBStore = MongoDBStore(session);
+const dbStore = new DBStore({ uri: "mongodb://localhost:27017/express-session-example", collection: "sessions" });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
-app.use(session({ secret: "superSecred", resave: false, saveUninitialized: true, cookie: { sameSite: false, secure: false, httpOnly: false } }));
+app.use(session({ secret: "superSecred", resave: true, saveUninitialized: true, cookie: { secure: false, httpOnly: false }, store: dbStore }));
 app.use(routes);
 
 const server = createServer(app);
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log("⚡️[SERVER]: RUNNING");
-    console.log(`⚡[PORT]: ${3000}`);
+    console.log(`⚡[PORT]: ${PORT}`);
     console.log("⚡️[MESSAGE]: エブリシングOK、頑張ってねー、エルトホルくん。ヽ(o＾▽＾o)ノ");
 });
