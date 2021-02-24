d910441bce38c10ab17fc5231b32f33e53a50883
Suprianto Djamalu <erthru@Supriantos-MacBook-Pro.local>
2021-02-25 00:48:59 +0800

    new origin added

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const PORT = 3003;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "http://127.0.0.1:3000"], credentials: true }));
 app.use(cookieParser());
 app.use(session({ secret: "superSecred", resave: false, saveUninitialized: false, cookie: { secure: false, httpOnly: true } }));
 app.use(routes);
